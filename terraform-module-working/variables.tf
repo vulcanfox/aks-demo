5c25ad204adab92eff22f79e27668d8124f2a678
Adnan <118762576+vulcanfox@users.noreply.github.com>
2025-10-28 08:00:06 +0000

    working cluster-provisioning-module

---
+++ terraform-module-working/variables.tf
@@ -0,0 +1,16 @@
+variable "generic_name" {
+  description = "Name to use for resources."
+  type        = string
+  default     = "adnan-demo"
+}
+
+variable "appId" {
+  type      = string
+  sensitive = true
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
