d63297113928c2a9f95191010a62e9507d14d874
Adnan <118762576+vulcanfox@users.noreply.github.com>
2025-10-28 13:26:21 +0000

    mlflow-tracker-on-aks-working

---
+++ modules/mlflow/main.tf
@@ -0,0 +1,27 @@
+resource "helm_release" "mlflow" {
+  name             = "mlflow"
+  repository       = "https://community-charts.github.io/helm-charts"
+  chart            = "mlflow"
+  namespace        = var.namespace
+  create_namespace = true
+  version          = "1.7.3"
+  timeout          = 900
+  wait             = true
+
+  values = [
+    yamlencode({
+      service = {
+        type = "LoadBalancer"
+      }
+
+      extraEnvVars = {
+        # Allow the browser to call MLflow API
+        MLFLOW_SERVER_CORS_ALLOWED_ORIGINS = "*" # for testing; prefer explicit origins
+
+        # Allow the Host header(s) you access MLflow with
+        # Example: set to "mlflow.example.com,10.0.0.*,localhost:*"
+        MLFLOW_SERVER_ALLOWED_HOSTS = "48.217.201.222" # change to your host(s) or use "*"
+      }
+    })
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -1,18 +1,21 @@
+variable "generic_name" {
+  description = "Name to use for resources."
+  type        = string
+  default     = "adnan-demo"
+}
 
 variable "appId" {
-  description = "Azure Kubernetes Service Cluster service principal"
+  type      = string
+  sensitive = true
 }
 
 variable "password" {
-  description = "Azure Kubernetes Service Cluster password"
+  type      = string
+  sensitive = true
 }
 
-variable "generic_name" {
-  default     = "adnan-aks-demo"
-  description = "The generic name to use for the project such as for resource group or resource name."
-}
-
-variable "kubernetes_version" {
-  default     = "1.26.3"
-  description = "The kubernetes version to use."
+variable "namespace" {
+  description = "Name to use for namespace."
+  type        = string
+  default     = "adnan-demo-namespace"
 }
