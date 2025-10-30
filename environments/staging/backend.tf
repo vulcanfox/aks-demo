13b3db819aff220e668d012f618d1cb5b0802ba3
Adnan <118762576+vulcanfox@users.noreply.github.com>
2025-10-30 18:59:48 +0000

    Update backend.tf

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstatestrgadnan"
     container_name       = "tfstate"
-    key                  = "staging/terraform.tfstate"
+    key                  = "staging/terraform.tfstate" # staging storage folder
   }
 }
