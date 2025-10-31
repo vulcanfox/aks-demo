393c8f89812b074786ecbc595154c7be5d813426
Adnan <118762576+vulcanfox@users.noreply.github.com>
2025-10-31 11:03:45 +0000

    Update main.tf

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -44,7 +44,7 @@ module "cluster" {
   source       = "../../modules/cluster"
   appId        = var.appId        # Pass root var to module
   password     = var.password     # Pass root var to module
-  generic_name = var.generic_name # Generic name
+  generic_name = var.generic_name # Generic name..
   environment  = var.environment
 }
 
