c0b384d239e9d9bbb167fb79a2f1d6d84b310f82
Adnan <118762576+vulcanfox@users.noreply.github.com>
2025-10-31 09:02:56 +0000

    Update main.tf

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -42,8 +42,8 @@ provider "helm" {
 
 module "cluster" {
   source       = "../../modules/cluster"
-  appId        = var.appId    # Pass root var to module..
-  password     = var.password # Pass root var to module...
+  appId        = var.appId    # Pass root var to module
+  password     = var.password # Pass root var to module
   generic_name = var.generic_name
   environment  = var.environment
 }
